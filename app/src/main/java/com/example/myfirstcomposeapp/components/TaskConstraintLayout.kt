cc8c69d9fc40b8921a013b56771f8e36847ecfef
Miguel Yengle <miguel.acuna@treinus.com.br>
2026-06-11 16:51:37 -0300

    constraint layout exercises

---
+++ app/src/main/java/com/example/myfirstcomposeapp/components/TaskConstraintLayout.kt
@@ -0,0 +1,71 @@
+package com.example.myfirstcomposeapp.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+
+
+@Composable
+fun TaskConstraintLayout(
+    modifier: Modifier
+) {
+
+    ConstraintLayout(
+        modifier = modifier.fillMaxSize()
+    ) {
+
+        val (boxOutCyan, boxOutBlack, boxOutDarkGray, boxOutBlue) = createRefs()
+        val (boxMagenta, boxGreen,  boxYellow,boxGray,boxRed) = createRefs()
+
+        Box(Modifier.size(150.dp).background(Color.Cyan).constrainAs(boxOutCyan){
+            end.linkTo(boxMagenta.end)
+            bottom.linkTo(boxMagenta.top)
+        })
+        Box(Modifier.size(75.dp).background(Color.Black).constrainAs(boxOutBlack){
+            start.linkTo(boxOutCyan.end)
+            top.linkTo(boxOutCyan.top)
+            bottom.linkTo(boxOutCyan.bottom)
+            end.linkTo(boxOutDarkGray.start)
+        })
+        Box(Modifier.size(150.dp).background(Color.DarkGray).constrainAs(boxOutDarkGray){
+            start.linkTo(boxGreen.start)
+            bottom.linkTo(boxGreen.top)
+        })
+
+        Box(Modifier.size(150.dp).background(Color.Blue).constrainAs(boxOutBlue){
+            top.linkTo(boxYellow.bottom)
+            start.linkTo(parent.start)
+            end.linkTo(parent.end)
+        })
+
+        Box(Modifier.size(75.dp).background(Color.Magenta).constrainAs(boxMagenta){
+            end.linkTo(boxYellow.start)
+            bottom.linkTo(boxYellow.top)
+        })
+        Box(Modifier.size(75.dp).background(Color.Green).constrainAs(boxGreen){
+            start.linkTo(boxYellow.end)
+            bottom.linkTo(boxYellow.top)
+        })
+        Box(Modifier.size(75.dp).background(Color.Yellow).constrainAs(boxYellow){
+            start.linkTo(parent.start)
+            end.linkTo(parent.end)
+            bottom.linkTo(parent.bottom)
+            top.linkTo(parent.top)
+        })
+        Box(Modifier.size(75.dp).background(Color.Gray).constrainAs(boxGray){
+            top.linkTo(boxYellow.bottom)
+            end.linkTo(boxYellow.start)
+        })
+        Box(Modifier.size(75.dp).background(Color.Red).constrainAs(boxRed){
+            top.linkTo(boxYellow.bottom)
+            start.linkTo(boxYellow.end)
+        })
+    }
+}
