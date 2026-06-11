501bac5a994bb69818a58958215cd663d8dbc6f6
Miguel Yengle <miguel.acuna@treinus.com.br>
2026-06-11 15:37:02 -0300

    package fiels

---
+++ app/src/main/java/com/example/myfirstcomposeapp/components/TaskBoxes.kt
@@ -0,0 +1,68 @@
+package com.example.myfirstcomposeapp.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun TaskBoxes(
+    modifier: Modifier
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+    ) {
+        Box(
+            modifier = Modifier
+                .background(Color.Cyan)
+                .fillMaxWidth()
+                .weight(1f),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(text = "Ejemplo 1")
+        }
+        Row(
+            modifier = Modifier
+                .background(Color.Transparent)
+                .weight(1f)
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.Red)
+                    .weight(1f),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(text = "Ejemplo 2")
+            }
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.Green)
+                    .weight(1f),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(text = "Ejemplo 3")
+            }
+        }
+        Box(
+            modifier = Modifier
+                .background(Color.Magenta)
+                .fillMaxWidth()
+                .weight(1f),
+            contentAlignment = Alignment.BottomCenter
+        ) {
+            Text(text = "Ejemplo 4")
+        }
+
+    }
+}
