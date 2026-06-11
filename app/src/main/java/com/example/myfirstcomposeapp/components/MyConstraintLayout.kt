37c22ebc692934f5d0bc7049aac4259042e5c34c
Miguel Yengle <miguel.acuna@treinus.com.br>
2026-06-11 16:22:54 -0300

    implemented constraint layouts

---
+++ app/src/main/java/com/example/myfirstcomposeapp/components/MyConstraintLayout.kt
@@ -0,0 +1,65 @@
+package com.example.myfirstcomposeapp.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+
+@Composable
+
+fun MyBasicConstraintLayout(
+    modifier: Modifier
+) {
+    ConstraintLayout(
+        modifier = modifier.fillMaxSize()
+    ) {
+        val (boxRed, boxGray, boxGreen, boxMagenta, boxYellow) = createRefs()
+        Box(
+            Modifier
+                .size(150.dp)
+                .background(Color.Red)
+                .constrainAs(boxRed) {
+                    top.linkTo(boxYellow.bottom)
+                    start.linkTo(boxYellow.end)
+                })
+        Box(
+            Modifier
+                .size(150.dp)
+                .background(Color.Gray)
+                .constrainAs(boxGray) {
+                    top.linkTo(boxYellow.bottom)
+                    end.linkTo(boxYellow.start)
+                })
+        Box(
+            Modifier
+                .size(150.dp)
+                .background(Color.Green)
+                .constrainAs(boxGreen) {
+                    bottom.linkTo(boxYellow.top)
+                    start.linkTo(boxYellow.end)
+                })
+        Box(
+            Modifier
+                .size(150.dp)
+                .background(Color.Magenta)
+                .constrainAs(boxMagenta) {
+                    bottom.linkTo(boxYellow.top)
+                    end.linkTo(boxYellow.start)
+                })
+        Box(
+            Modifier
+                .size(150.dp)
+                .background(Color.Yellow)
+                .constrainAs(boxYellow) {
+                    bottom.linkTo(parent.bottom)
+                    end.linkTo(parent.end)
+                    top.linkTo(parent.top)
+                    start.linkTo(parent.start)
+                })
+    }
+}
