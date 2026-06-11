f352e922472c95d25fe6c0eb16acacd163ee5af8
Miguel Yengle <miguel.acuna@treinus.com.br>
2026-06-11 14:08:48 -0300

    basic modifiers

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,16 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.myfirstcomposeapp"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.myfirstcomposeapp"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
--- app/src/main/java/com/example/myfirstcomposeapp/login/LoginScreen.kt
+++ app/src/main/java/com/example/myfirstcomposeapp/login/LoginScreen.kt
@@ -1,10 +1,20 @@
 package com.example.myfirstcomposeapp.login
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 
 @Composable
@@ -33,7 +43,40 @@ fun ExamplePreview() {
 
 @Composable
 fun Example(
-    text: String = ""
+    text: String
 ) {
-    Text(text = "Hello $text")
+    Text(
+        text = "Hello $text",
+        modifier = Modifier.height(33.dp).width(70.dp)
+    )
+    Text(
+        text = "Hello fixed size $text",
+        modifier = Modifier.size(60.dp)
+    )
+    Text(
+        text = "Hello ocupa toda la pantalla $text",
+        modifier = Modifier.fillMaxWidth().fillMaxHeight()
+    )
+    Text(
+        text = "Hello ocupa todo $text",
+        modifier = Modifier.fillMaxSize()
+    )
+    Text(
+        text = "Hello paddinfg $text",
+        modifier = Modifier.padding(all = 40.dp)
+    )
+    Text(
+        text = "Hello paddinfg $text",
+        modifier = Modifier.padding(top = 30.dp)
+    )
+    Text(
+        text = "Hello paddinfg $text",
+        modifier = Modifier.padding(horizontal = 30.dp)
+    )
+    Text(
+        text = "Hello paddinfg $text",
+        modifier = Modifier
+            .background(Color.Green)
+            .padding(horizontal = 30.dp)
+    )
 }
