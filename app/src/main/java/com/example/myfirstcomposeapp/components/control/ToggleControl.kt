e987f3da2ec6a4b87b86488498ee2e0f2624d1ce
Miguel Yengle <miguel.acuna@treinus.com.br>
2026-06-16 11:04:08 -0300

    arrange structure

--- app/src/main/java/com/example/myfirstcomposeapp/components/control/ToggleControl.kt
+++ app/src/main/java/com/example/myfirstcomposeapp/components/control/ToggleControl.kt
@@ -1,6 +1,7 @@
 package com.example.myfirstcomposeapp.components.control
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Switch
@@ -20,73 +21,76 @@ import com.example.myfirstcomposeapp.R
 fun MySwitch(
     modifier: Modifier = Modifier
 ) {
-
     var switchState by remember { mutableStateOf(true) }
-
     Column(
         modifier = modifier.fillMaxSize(),
     ) {
-        Switch(
-            checked = switchState,
-            onCheckedChange = { switchState = it }
-        )
-        Switch(
-            checked = switchState,
-            onCheckedChange = { switchState = it },
-            colors = SwitchDefaults.colors(
-                //bolita
-                checkedThumbColor = Color.Red,
-                uncheckedThumbColor = Color.Blue,
-                //icons
-                checkedIconColor = Color.Green,
-                uncheckedIconColor = Color.Cyan,
-                //border
-                checkedBorderColor = Color.Magenta,
-                uncheckedBorderColor = Color.Black,
-                //track
-                checkedTrackColor = Color.White,
-                uncheckedTrackColor = Color.LightGray
+        Text("Switch - toggle")
+        Row() {
+            Switch(
+                checked = switchState,
+                onCheckedChange = { switchState = it }
             )
-        )
-        Switch(
-            checked = switchState,
-            onCheckedChange = { switchState = it },
-            enabled = false,
-            colors = SwitchDefaults.colors(
-                //bolita
-                disabledCheckedThumbColor = Color.Yellow,
-                disabledUncheckedThumbColor = Color.Cyan,
-                //icons
-                disabledCheckedIconColor = Color.Green,
-                disabledUncheckedIconColor = Color.Red,
-                //border
-                disabledCheckedBorderColor = Color.Gray,
-                disabledUncheckedBorderColor = Color.DarkGray,
-                //track
-                disabledCheckedTrackColor = Color.Magenta,
-                disabledUncheckedTrackColor = Color.LightGray
+            Switch(
+                checked = switchState,
+                onCheckedChange = { switchState = it },
+                thumbContent = { Text("switch text") }
             )
-        )
-        Switch(
-            checked = switchState,
-            onCheckedChange = { switchState = it },
-            thumbContent = { Text("switch text") }
-        )
-        Switch(
-            checked = switchState,
-            onCheckedChange = { switchState = it },
-            thumbContent = { Text("switch text") },
-            enabled = false
-        )
-        Switch(
-            checked = switchState,
-            onCheckedChange = { switchState = it },
-            thumbContent = {
-                Icon(
-                    painter = painterResource(R.drawable.ic_menu_profile),
-                    contentDescription = "icono switch"
+            Switch(
+                checked = switchState,
+                onCheckedChange = { switchState = it },
+                thumbContent = {
+                    Icon(
+                        painter = painterResource(R.drawable.ic_menu_profile),
+                        contentDescription = "icono switch"
+                    )
+                }
+            )
+        }
+        Row() {
+            Switch(
+                checked = switchState,
+                onCheckedChange = { switchState = it },
+                colors = SwitchDefaults.colors(
+                    //bolita
+                    checkedThumbColor = Color.Red,
+                    uncheckedThumbColor = Color.Blue,
+                    //icons
+                    checkedIconColor = Color.Green,
+                    uncheckedIconColor = Color.Cyan,
+                    //border
+                    checkedBorderColor = Color.Magenta,
+                    uncheckedBorderColor = Color.Black,
+                    //track
+                    checkedTrackColor = Color.White,
+                    uncheckedTrackColor = Color.LightGray
                 )
-            }
-        )
+            )
+            Switch(
+                checked = switchState,
+                onCheckedChange = { switchState = it },
+                enabled = false,
+                colors = SwitchDefaults.colors(
+                    //bolita
+                    disabledCheckedThumbColor = Color.Yellow,
+                    disabledUncheckedThumbColor = Color.Cyan,
+                    //icons
+                    disabledCheckedIconColor = Color.Green,
+                    disabledUncheckedIconColor = Color.Red,
+                    //border
+                    disabledCheckedBorderColor = Color.Gray,
+                    disabledUncheckedBorderColor = Color.DarkGray,
+                    //track
+                    disabledCheckedTrackColor = Color.Magenta,
+                    disabledUncheckedTrackColor = Color.LightGray
+                )
+            )
+            Switch(
+                checked = switchState,
+                onCheckedChange = { switchState = it },
+                thumbContent = { Text("switch text") },
+                enabled = false
+            )
+        }
     }
 }
